41a3217b8f526812da3d14b23afddb1a2ee9e6e3
Nicolas Balaudo <nicobalaudo@gmail.com>
2022-06-21 16:01:42 -0300

    Agregado modificación DOM

--- js/index.js
+++ js/index.js
@@ -1,25 +1,32 @@
 function prestamos() {
-    let ingresosAnuales = prompt("Indique sus ingresos anuales, los cuales deben ser mayores a 1 Millón de pesos.");
+    let ingresosAnuales = prompt(`Indicá tus ingresos anuales, los cuales deben ser mayores a 1 Millón de pesos.`)
     if (ingresosAnuales >= 1000000) {
-        const tipoPrestamos = ["Personal", "Empresarial"];
-        let userIndex = -1;
-        do {
-            let prestamoSeleccionado = prompt("Especifique el tipo de prestamo: "+tipoPrestamos.join(", "))
-            for (let i = 0; i < tipoPrestamos.length; i++) {
-                if (tipoPrestamos[i] === prestamoSeleccionado) {
-                    userIndex = i;
-                    break;
+        let edadCliente = prompt(`Indicá tu edad, recordá que debés ser mayor de edad.`)
+        if (edadCliente >= 18) {
+            const tipoPrestamos = [`Personal`, `Empresarial`, `Estudios`, `Hipotecarios`]
+            let userIndex = -1;
+            do {
+                let prestamoSeleccionado = prompt(`Especicá el tipo de prestamo: `+tipoPrestamos.join(`, `))
+                for (let i = 0; i < tipoPrestamos.length; i++) {
+                    if (tipoPrestamos[i] === prestamoSeleccionado) {
+                        userIndex = i;
+                        break;
+                    }
                 }
-            }
-            if (userIndex > -1) {
-                alert("Felicitaciones! Tiene aprobado su préstamo.")
-                alert("Puede retirar el dinero en la sucursal más cercana llevando comprobantes de sueldo que avalen los ingresos que usted indicó.")
-            } else {
-                alert("Usted ingresó un dato equivocado, pruebe ingresar el tipo de préstamo respetando las mayúsculas inciales.");
-            }
+                if (userIndex > -1) {
+                    alert(`Felicitaciones! Tenés aprobado tu préstamo.`)
+                    alert(`Podés retirar el dinero en la sucursal más cercana llevando comprobantes de sueldo que avalen los ingresos que indicaste.`)
+                    document.getElementById(`maindiv`).innerHTML = `<h2>¡¡FELICITACIONES!! Corré al banco ¡YA!</h2>`
+                } else {
+                    alert(`Ingresaste un dato equivocado, probá ingresando el tipo de préstamo respetando las mayúsculas inciales.`)
+                }
+            } while (userIndex === -1);
+        } else {
+                alert(`Lo sentimos, pero no cumples con los requisitos mínimos para aplicar a este préstamo.`)
+                document.getElementById(`maindiv`).innerHTML = `<h2>Lo sentimos, no cumples la edad mínima requerida :'(</h2><p>Para reintentarlo recarga la pagina</p>`
         }
-        while (userIndex === -1);
-    }   else {
-            alert("Lo sentimos, pero no cumples con los requisitos mínimos para aplicar a este préstamo.");
-        }
+    } else {
+            alert(`Lo sentimos, pero no cumples con los requisitos mínimos para aplicar a este préstamo.`)
+            document.getElementById(`maindiv`).innerHTML = `<h2>Lo sentimos, no cumples los ingresos mínimos requeridos :'(</h2><p>Para reintentarlo recarga la pagina</p>`
+    }
 }
