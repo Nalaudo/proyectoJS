33a4bc427109bdd602687f558c536664bf4ceef3
Nicolas Balaudo <nicobalaudo@gmail.com>
2022-06-21 19:24:03 -0300

    Elimine carpeta páginas y agregué eventos

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-function prestamos() {
+document.getElementById(`onclick`).onclick = botonPrestamo
+document.getElementById(`onclick2`).onclick = botonPrestamo
+function botonPrestamo() {
+    document.getElementById(`prestamo`).innerHTML = `<h2 id="prestamo2">HAGA CLICK AQUÍ PARA OBTENER SU PRÉSTAMO ¡YA!</h2>`
+    document.getElementById(`prestamo2`).onclick = formPrestamos 
+}
+function formPrestamos() {
     let ingresosAnuales = prompt(`Indicá tus ingresos anuales, los cuales deben ser mayores a 1 Millón de pesos.`)
     if (ingresosAnuales >= 1000000) {
         let edadCliente = prompt(`Indicá tu edad, recordá que debés ser mayor de edad.`)
@@ -16,17 +22,17 @@ function prestamos() {
                 if (userIndex > -1) {
                     alert(`Felicitaciones! Tenés aprobado tu préstamo.`)
                     alert(`Podés retirar el dinero en la sucursal más cercana llevando comprobantes de sueldo que avalen los ingresos que indicaste.`)
-                    document.getElementById(`maindiv`).innerHTML = `<h2>¡¡FELICITACIONES!! Corré al banco ¡YA!</h2>`
+                    document.getElementById(`prestamo`).innerHTML = `<h2>¡¡FELICITACIONES!! Corré al banco ¡YA!</h2>`
                 } else {
                     alert(`Ingresaste un dato equivocado, probá ingresando el tipo de préstamo respetando las mayúsculas inciales.`)
                 }
             } while (userIndex === -1);
         } else {
                 alert(`Lo sentimos, pero no cumples con los requisitos mínimos para aplicar a este préstamo.`)
-                document.getElementById(`maindiv`).innerHTML = `<h2>Lo sentimos, no cumples la edad mínima requerida :'(</h2><p>Para reintentarlo recarga la pagina</p>`
+                document.getElementById(`prestamo`).innerHTML = `<h2>Lo sentimos, no cumples la edad mínima requerida :'(</h2><p>Para reintentarlo recarga la pagina</p>`
         }
     } else {
             alert(`Lo sentimos, pero no cumples con los requisitos mínimos para aplicar a este préstamo.`)
-            document.getElementById(`maindiv`).innerHTML = `<h2>Lo sentimos, no cumples los ingresos mínimos requeridos :'(</h2><p>Para reintentarlo recarga la pagina</p>`
+            document.getElementById(`prestamo`).innerHTML = `<h2>Lo sentimos, no cumples los ingresos mínimos requeridos :'(</h2><p>Para reintentarlo recarga la pagina</p>`
     }
 }
