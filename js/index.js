8b91b8f86d30f7485ac2346d9c6fc77235a6e81b
Nicolas Balaudo <nicobalaudo@gmail.com>
2022-06-30 20:12:20 -0300

    Storage y JSON agregados

--- js/index.js
+++ js/index.js
@@ -7,9 +7,11 @@ function botonPrestamo() {
 function formPrestamos() {
     let ingresosAnuales = prompt(`Indicá tus ingresos anuales, los cuales deben ser mayores a 1 Millón de pesos.`)
     if (ingresosAnuales >= 1000000) {
-        let edadCliente = prompt(`Indicá tu edad, recordá que debés ser mayor de edad.`)
-        if (edadCliente >= 18) {
+        let edadCliente = prompt(`Indicá tu edad, recordá que debés ser mayor de edad y no puedes tener más de 70 años.`)
+        if ((edadCliente >= 18) && (edadCliente <= 70)) {
             const tipoPrestamos = [`Personal`, `Empresarial`, `Estudios`, `Hipotecarios`]
+            const enJSON = (JSON.stringify(tipoPrestamos))
+            console.log(enJSON) //es para que el desarrollador pueda ver por la consola que el array es correcto (no se me ocurría en que usar el JSON)
             let userIndex = -1;
             do {
                 let prestamoSeleccionado = prompt(`Especicá el tipo de prestamo: `+tipoPrestamos.join(`, `))
@@ -20,19 +22,23 @@ function formPrestamos() {
                     }
                 }
                 if (userIndex > -1) {
-                    alert(`Felicitaciones! Tenés aprobado tu préstamo.`)
-                    alert(`Podés retirar el dinero en la sucursal más cercana llevando comprobantes de sueldo que avalen los ingresos que indicaste.`)
-                    document.getElementById(`prestamo`).innerHTML = `<h2>¡¡FELICITACIONES!! Corré al banco ¡YA!</h2>`
+                    sessionStorage.setItem(`formPrestamos`, `1`)
                 } else {
                     alert(`Ingresaste un dato equivocado, probá ingresando el tipo de préstamo respetando las mayúsculas inciales.`)
                 }
             } while (userIndex === -1);
         } else {
-                alert(`Lo sentimos, pero no cumples con los requisitos mínimos para aplicar a este préstamo.`)
-                document.getElementById(`prestamo`).innerHTML = `<h2>Lo sentimos, no cumples la edad mínima requerida :'(</h2><p>Para reintentarlo recarga la pagina</p>`
+                sessionStorage.setItem(`formPrestamos`, `2`)
         }
     } else {
-            alert(`Lo sentimos, pero no cumples con los requisitos mínimos para aplicar a este préstamo.`)
-            document.getElementById(`prestamo`).innerHTML = `<h2>Lo sentimos, no cumples los ingresos mínimos requeridos :'(</h2><p>Para reintentarlo recarga la pagina</p>`
+            sessionStorage.setItem(`formPrestamos`, `3`)
+    }
+    if (sessionStorage.getItem(`formPrestamos`) === `1`) {
+        document.getElementById(`prestamo`).innerHTML = `<h2>¡¡FELICITACIONES!! Corré al banco ¡YA!</h2>`
+        alert(`Felicitaciones! Tenés aprobado tu préstamo.`)
+        alert(`Podés retirar el dinero en la sucursal más cercana llevando comprobantes de sueldo que avalen los ingresos que indicaste.`)
+    } else {
+        document.getElementById(`prestamo`).innerHTML = `<h2>Lo sentimos, pero no cumples con los requisitos mínimos para aplicar a este préstamo. :'(</h2>`
+        alert(`Lo sentimos, pero no cumples con los requisitos mínimos para aplicar a este préstamo.`)
     }
 }
