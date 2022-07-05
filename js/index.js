1e16985d43654807588d3d2bcc17cdb8cece1df3
Nicolas Balaudo <nicobalaudo@gmail.com>
2022-07-05 20:27:35 -0300

    Optimización del código

--- js/index.js
+++ js/index.js
@@ -6,39 +6,34 @@ function botonPrestamo() {
 }
 function formPrestamos() {
     let ingresosAnuales = prompt(`Indicá tus ingresos anuales, los cuales deben ser mayores a 1 Millón de pesos.`)
-    if (ingresosAnuales >= 1000000) {
-        let edadCliente = prompt(`Indicá tu edad, recordá que debés ser mayor de edad y no puedes tener más de 70 años.`)
+    let edadCliente = false
+    ingresosAnuales >= 1000000 ? edadCliente = prompt(`Indicá tu edad, recordá que debés ser mayor de edad y no puedes tener más de 70 años.`) : sessionStorage.setItem(`formPrestamos`, `3`)
         if ((edadCliente >= 18) && (edadCliente <= 70)) {
-            const tipoPrestamos = [`Personal`, `Empresarial`, `Estudios`, `Hipotecarios`]
-            const enJSON = (JSON.stringify(tipoPrestamos))
-            console.log(enJSON) //es para que el desarrollador pueda ver por la consola que el array es correcto (no se me ocurría en que usar el JSON)
+            const tipoPrestamos1 = [`Personal`, `Hipotecario`]
+            const tipoPrestamos2 = [`Empresarial`, `Estudio`]
+            const tipoPrestamos = [...tipoPrestamos1, ...tipoPrestamos2]
+            console.log(tipoPrestamos)
             let userIndex = -1;
             do {
-                let prestamoSeleccionado = prompt(`Especicá el tipo de prestamo: `+tipoPrestamos.join(`, `))
+                let prestamoSeleccionado = prompt(`Especificá el tipo de prestamo: `+ tipoPrestamos.join(", "))
                 for (let i = 0; i < tipoPrestamos.length; i++) {
                     if (tipoPrestamos[i] === prestamoSeleccionado) {
                         userIndex = i;
                         break;
                     }
                 }
-                if (userIndex > -1) {
-                    sessionStorage.setItem(`formPrestamos`, `1`)
-                } else {
-                    alert(`Ingresaste un dato equivocado, probá ingresando el tipo de préstamo respetando las mayúsculas inciales.`)
-                }
+                userIndex > -1 && sessionStorage.setItem(`formPrestamos`, `1`)
             } while (userIndex === -1);
         } else {
                 sessionStorage.setItem(`formPrestamos`, `2`)
         }
-    } else {
-            sessionStorage.setItem(`formPrestamos`, `3`)
-    }
-    if (sessionStorage.getItem(`formPrestamos`) === `1`) {
-        document.getElementById(`prestamo`).innerHTML = `<h2>¡¡FELICITACIONES!! Corré al banco ¡YA!</h2>`
-        alert(`Felicitaciones! Tenés aprobado tu préstamo.`)
-        alert(`Podés retirar el dinero en la sucursal más cercana llevando comprobantes de sueldo que avalen los ingresos que indicaste.`)
-    } else {
-        document.getElementById(`prestamo`).innerHTML = `<h2>Lo sentimos, pero no cumples con los requisitos mínimos para aplicar a este préstamo. :'(</h2>`
-        alert(`Lo sentimos, pero no cumples con los requisitos mínimos para aplicar a este préstamo.`)
-    }
+    let aprobado = sessionStorage.getItem(`formPrestamos`) === `1` && `<h2>¡¡FELICITACIONES!! Corré al banco ¡YA!</h2>`
+    document.getElementById(`prestamo`).innerHTML = (aprobado || `<h2>Lo sentimos, pero no cumples con los requisitos mínimos para aplicar a este préstamo. :'(</h2>`)
 }
+//usuarios (solo admin, sitio en desarrollo, solo pruebas)
+const usuarios = {
+    nombre: "Admin",
+    edad: 18
+}
+const {nombre, edad} = usuarios
+console.log("Nombre: " + nombre +", "+ "Edad: "+ edad)
