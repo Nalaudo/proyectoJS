79dac291131a50ad9ca36e0fb87b6e97dfcadb05
Nicolas Balaudo <nicobalaudo@gmail.com>
2022-07-07 12:02:32 -0300

    Agrego toastify

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 document.getElementById(`onclick`).onclick = botonPrestamo
 document.getElementById(`onclick2`).onclick = botonPrestamo
 function botonPrestamo() {
+    Toastify({
+        text: "Ya estás más cerca de obtener tu préstamo!",
+        duration: 3000,
+        }).showToast();
     document.getElementById(`prestamo`).innerHTML = `<h2 id="prestamo2">HAGA CLICK AQUÍ PARA OBTENER SU PRÉSTAMO ¡YA!</h2>`
-    document.getElementById(`prestamo2`).onclick = formPrestamos 
+    document.getElementById(`prestamo2`).onclick = formPrestamos
 }
 function formPrestamos() {
     let ingresosAnuales = prompt(`Indicá tus ingresos anuales, los cuales deben ser mayores a 1 Millón de pesos.`)
@@ -13,22 +17,39 @@ function formPrestamos() {
             const tipoPrestamos2 = [`Empresarial`, `Estudio`]
             const tipoPrestamos = [...tipoPrestamos1, ...tipoPrestamos2]
             console.log(tipoPrestamos)
-            let userIndex = -1;
+            let userIndex = -1
             do {
                 let prestamoSeleccionado = prompt(`Especificá el tipo de prestamo: `+ tipoPrestamos.join(", "))
                 for (let i = 0; i < tipoPrestamos.length; i++) {
                     if (tipoPrestamos[i] === prestamoSeleccionado) {
-                        userIndex = i;
-                        break;
+                        userIndex = i
+                        break
                     }
                 }
                 userIndex > -1 && sessionStorage.setItem(`formPrestamos`, `1`)
-            } while (userIndex === -1);
+            } while (userIndex === -1)
         } else {
                 sessionStorage.setItem(`formPrestamos`, `2`)
         }
+    let toastAprobado = 
+    sessionStorage.getItem(`formPrestamos`) === `1` ? 
+    Toastify({
+        text: "Préstamo aprobado :)", 
+        duration: 3000, 
+        style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      }
+    }).showToast() 
+    : Toastify({
+        text: "Prestamo desaprobado :(", 
+        duration: 3000,
+        style: {
+            background: "linear-gradient(to right, #e26923, #ff0000)",
+          }
+    }).showToast()
     let aprobado = sessionStorage.getItem(`formPrestamos`) === `1` && `<h2>¡¡FELICITACIONES!! Corré al banco ¡YA!</h2>`
-    document.getElementById(`prestamo`).innerHTML = (aprobado || `<h2>Lo sentimos, pero no cumples con los requisitos mínimos para aplicar a este préstamo. :'(</h2>`)
+    let desaprobado = sessionStorage.getItem(`formPrestamos`) != `1` && `<h2>Lo sentimos, pero no cumples con los requisitos mínimos para aplicar a este préstamo. :'(</h2>`
+    document.getElementById(`prestamo`).innerHTML = (aprobado || desaprobado)
 }
 //usuarios (solo admin, sitio en desarrollo, solo pruebas)
 const usuarios = {
